c2043ff03fee400b5541306884886693c97c2492
Munh-Orgil416 <m.orgilmn@gmail.com>
2023-05-28 13:07:53 +0800

    test

--- src/backend/database/database.java
+++ src/backend/database/database.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 public class database {
     static final String DB_URL = "jdbc:mysql://localhost:3306/exammr";
-    static final String USER = "root";
+    static final String USER = "test";
     static final String PASS = "3B5GqPjpDr5t";
 
     public static Connection DBConn;
