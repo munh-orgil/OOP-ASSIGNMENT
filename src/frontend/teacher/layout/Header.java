6bff921030034d770c4b87d29713f2a7905088f2
Munh-Orgil416 <m.orgilmn@gmail.com>
2023-05-28 16:09:46 +0800

    header

--- src/frontend/teacher/layout/Header.java
+++ src/frontend/teacher/layout/Header.java
@@ -4,18 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.MenuBar;
-import java.awt.Shape;
-import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import frontend.modules.Modules;
@@ -53,87 +42,21 @@ public class Header {
         myPanel.add(imageLabel1);
         header.add(myPanel, BorderLayout.EAST);
 
-        // try {
-        //     JPanel profile = new RoundImage(Modules.login.ProfileImagePath);
-        //     header.add(profile, BorderLayout.EAST);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
-        
-              // Create menu bar
-             
-              JMenuBar menuBar = new JMenuBar();
-              menuBar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-              header.add(menuBar, BorderLayout.EAST);
-      
-              // Create menu
-              JMenu menu = new JMenu("Username");
-              menuBar.add(menu);
-      
-              // Create menu items
-              JMenuItem item1 = new JMenuItem("Profile");
-              JMenuItem item2 = new JMenuItem("Log Out");
-
-            //   //actionListener
-            //   item2.addActionListener(new ActionListener() {
-            //     public void actionPerformed(ActionEvent e) {
-            //         // Perform logout operation
-            //         // Here, assuming you have a reference to the main application frame
-            //         JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(header);
-            //         frame.setContentPane(new LoginPanel()); // Set the login panel as the content pane
-            //         frame.pack(); // Pack the frame to update its layout
-            //     }
             
-            // });
-              // Add menu items to menu
-              menu.add(item1);
-              menu.add(item2);
-              // Set username in the menu
-              if (Modules.login != null) {
-                  menu.setText(Modules.login.Username);
-              }
-          }
-      
-        
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        header.add(menuBar, BorderLayout.EAST);
 
+        JMenu menu = new JMenu("Username");
+        menuBar.add(menu);
+
+        JMenuItem item1 = new JMenuItem("Profile");
+        JMenuItem item2 = new JMenuItem("Log Out");
+
+        menu.add(item1);
+        menu.add(item2);
+        if (Modules.login != null) {
+            menu.setText(Modules.login.Username);
+        }
     }
-    // public class RoundImage extends JPanel {
-    //     private BufferedImage image;
-    
-    //     public RoundImage(String imagePath) throws IOException {
-    //         image = ImageIO.read(new File(imagePath));
-    //     }
-    
-    //     @Override
-    //     protected void paintComponent(Graphics g) {
-    //         super.paintComponent(g);
-    
-    //         Graphics2D g2d = (Graphics2D) g.create();
-    //         Shape clip = new Ellipse2D.Float(0, 0, 80, 80);
-    //         g2d.setClip(clip);
-    
-    //         g2d.drawImage(image, 0, 0, 80, 80, null);
-    //         g2d.dispose();
-    //     }
-    // }
-
-    // public static BufferedImage makeRoundedCorner(BufferedImage image, int cornerRadius) {
-    //     int w = image.getWidth();
-    //     int h = image.getHeight();
-    //     BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-
-    //     Graphics2D g2 = output.createGraphics();
-
-    //     g2.setComposite(AlphaComposite.Src);
-    //     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    //     g2.setColor(Color.WHITE);
-    //     g2.fill(new RoundRectangle2D.Float(0, 0, w, h, cornerRadius, cornerRadius));
-
-    //     g2.setComposite(AlphaComposite.SrcAtop);
-    //     g2.drawImage(image, 0, 0, null);
-
-    //     g2.dispose();
-
-    //     return output;
-    // }
-
+}
